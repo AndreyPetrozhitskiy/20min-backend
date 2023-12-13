1fb13fd473a9a9f962a3760c6b1cef5b532f803a
Andrey <super.okha2018@yandex.ru>
2023-12-14 00:27:36 +1000

    final2

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool
 const pool = new Pool(
     {
-        user:"postgres",
-        password: "159357",
+        user:"developer",
+        password: "testpassword",
         host:'localhost',
         port: 5432 ,
-        database: "back-minute20"
+        database: "minute20"
     }
 )
 
