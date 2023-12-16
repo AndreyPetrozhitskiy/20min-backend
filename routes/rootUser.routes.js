06213ad8159ba81fc1be9a9574a636680bb54c4b
Andrey <super.okha2018@yandex.ru>
2023-12-16 19:18:24 +1000

    final7

--- routes/rootUser.routes.js
+++ routes/rootUser.routes.js
@@ -3,11 +3,7 @@ const rootUserContoller = require('../controller/rootuser.controller')
 const {check} = require("express-validator")
 
 const router = new Router()
-// Регистрация
-router.post('/registration',[
-    check('name',"Имя пользователя не может быть пустым").notEmpty(),
-    check('password',"Пароль не должен быть пустым , не меньше 5 символов или больше 100 символов").isLength({min:5,max:100})
-],rootUserContoller.registrationUser) 
+
 // Авторизация
 
 router.post('/login',[
