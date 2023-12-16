78b68be9988979194c21415ad6f6fc68f851de60
Andrey <super.okha2018@yandex.ru>
2023-12-16 19:34:56 +1000

    final7

--- config.js
+++ config.js
@@ -1,3 +1,29 @@
-module.exports ={
-    secret: "awd"
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+
+let jwtSecret = generateNewSecret();
+let jwtExpirationTime = 3600; // Время истечения срока действия токена в секундах (1 час)
+
+updateSecretAndExpirationTimer();
+
+function generateNewSecret() {
+  // Генерация случайной фразы в качестве нового секретного ключа
+  return crypto.randomBytes(32).toString('hex');
 }
+
+function updateSecretAndExpirationTimer() {
+  setTimeout(() => {
+    jwtSecret = generateNewSecret(); // Обновление секретного ключа
+    jwtExpirationTime = 7200; // Обновление времени истечения срока действия токена через 2 часа
+    updateSecretAndExpirationTimer(); // Планирование следующего обновления через 2 часа
+  }, 7200 * 1000); // 2 часа в миллисекундах
+}
+
+module.exports = {
+  get jwtSecret() {
+    return jwtSecret;
+  },
+  get jwtExpirationTime() {
+    return jwtExpirationTime;
+  },
+};
--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,61 +1,139 @@
 const db  = require("../db.js")
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const {validationResult} = require('express-validator')
 const path = require('path');
-
+const config = require('../config.js');
 class UserContoroller {
-    async registrationUser(req,res){
-        try {
-            const errors = validationResult(req)
-            if(!errors.isEmpty()) {
-                return res.status(400).json(errors.errors.map((e,index)=>(`${index}. Ошибка:${e.msg}`)))
-            }
-            const  {name,password} = req.body
-            const result = await db.query('SELECT * FROM users WHERE username = $1', [name]);
-            if (result.rows.length > 0) {
-                const isValidPassword = bcrypt.compareSync(password, result.rows[0].password);
-                if (isValidPassword) {
-                  // Пользователь ввел правильный пароль
-                  return res.status(400).json({
-                    error: 'Вы уже зарегистрированы. Пожалуйста, авторизуйтесь.',
-                  });
-                } else {
-                  // Пользователь ввел неправильный пароль
-                  return res.status(400).json({
-                    error: 'Пользователь с таким именем уже существует. Пожалуйста, выберите другое имя.',
-                  });
-                }
-              }
-            const hashPassword  = bcrypt.hashSync(password, 7);
-            const newUser = await db.query('INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *', [name, hashPassword]);
-            res.json(newUser.rows[0]);
+    async generateToken(user) {
+        const payload = {
+          userId: user.UserID, // Предполагается, что ваш объект пользователя имеет свойство 'UserID'
+          username: user.username,
+          // Добавьте любые дополнительные данные, которые вы хотите включить в токен
+        };
+    
+        const token = jwt.sign(payload, config.jwtSecret, { expiresIn: config.jwtExpirationTime });
+        return token;
+      }
+    // async registrationUser(req,res){
+    //     try {
+    //         const errors = validationResult(req)
+    //         if(!errors.isEmpty()) {
+    //             return res.status(400).json(errors.errors.map((e,index)=>(`${index}. Ошибка:${e.msg}`)))
+    //         }
+    //         const  {name,password} = req.body
+    //         const result = await db.query('SELECT * FROM users WHERE username = $1', [name]);
+    //         if (result.rows.length > 0) {
+    //             const isValidPassword = bcrypt.compareSync(password, result.rows[0].password);
+    //             if (isValidPassword) {
+    //               // Пользователь ввел правильный пароль
+    //               return res.status(400).json({
+    //                 error: 'Вы уже зарегистрированы. Пожалуйста, авторизуйтесь.',
+    //               });
+    //             } else {
+    //               // Пользователь ввел неправильный пароль
+    //               return res.status(400).json({
+    //                 error: 'Пользователь с таким именем уже существует. Пожалуйста, выберите другое имя.',
+    //               });
+    //             }
+    //           }
+    //         const hashPassword  = bcrypt.hashSync(password, 7);
+    //         const newUser = await db.query('INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *', [name, hashPassword]);
+    //         res.json(newUser.rows[0]);
 
-        } catch (e) {
-            console.log(`Ошибка: ${e.message}`);
-            res.status(400).json(`Ошибка: ${e.message}`);
-        }
-    }
-    async loginUser(req,res){
+    //     } catch (e) {
+    //         console.log(`Ошибка: ${e.message}`);
+    //         res.status(400).json(`Ошибка: ${e.message}`);
+    //     }
+    // }
+    // async loginUser(req,res){
+    //     try {
+    //         const errors = validationResult(req)
+    //         if(!errors.isEmpty()) {
+    //             return res.status(400).json(errors.errors.map((e,index)=>(`${index}. Ошибка:${e.msg}`)))
+    //         }
+    //         const  {name,password} = req.body
+    //         const nameSearch = await db.query('SELECT * FROM users WHERE username = $1', [name]);
+    //         if(nameSearch.rows.length === 0){
+    //             return res.status(400).json({error: `Пользователь ${name} не найден`})
+    //         }
+    //         const validPassword = bcrypt.compareSync(password,nameSearch.rows[0].password )
+    //         if (!validPassword) {
+    //             return res.status(400).json({error: `Введен неверный пароль`})
+    //         }
+    //         res.json("Вы успешно авторизованы")
+    // } catch (e) {
+    //     console.log(`Ошибка: ${e.message}`);
+    //     res.status(400).json(`Ошибка: ${e.message}`);
+    // }
+    // }
+    async registrationUser(req, res) {
         try {
-            const errors = validationResult(req)
-            if(!errors.isEmpty()) {
-                return res.status(400).json(errors.errors.map((e,index)=>(`${index}. Ошибка:${e.msg}`)))
-            }
-            const  {name,password} = req.body
-            const nameSearch = await db.query('SELECT * FROM users WHERE username = $1', [name]);
-            if(nameSearch.rows.length === 0){
-                return res.status(400).json({error: `Пользователь ${name} не найден`})
-            }
-            const validPassword = bcrypt.compareSync(password,nameSearch.rows[0].password )
-            if (!validPassword) {
-                return res.status(400).json({error: `Введен неверный пароль`})
-            }
-            res.json("Вы успешно авторизованы")
-    } catch (e) {
-        console.log(`Ошибка: ${e.message}`);
-        res.status(400).json(`Ошибка: ${e.message}`);
-    }
-    }
+          const errors = validationResult(req);
+          if (!errors.isEmpty()) {
+            return res.status(400).json(errors.errors.map((e, index) => `${index}. Ошибка:${e.msg}`));
+          }
+    
+          const { name, password } = req.body;
+          const result = await db.query('SELECT * FROM users WHERE username = $1', [name]);
+    
+          if (result.rows.length > 0) {
+                        const isValidPassword = bcrypt.compareSync(password, result.rows[0].password);
+                        if (isValidPassword) {
+                          // Пользователь ввел правильный пароль
+                          return res.status(400).json({
+                            error: 'Вы уже зарегистрированы. Пожалуйста, авторизуйтесь.',
+                          });
+                        } else {
+                          // Пользователь ввел неправильный пароль
+                          return res.status(400).json({
+                            error: 'Пользователь с таким именем уже существует. Пожалуйста, выберите другое имя.',
+                          });
+                        }
+                      }
+    
+          const hashPassword = bcrypt.hashSync(password, 7);
+          const newUser = await db.query('INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *', [name, hashPassword]);
+    
+          // Генерация JWT-токена после успешной регистрации
+          const token = await this.generateToken(newUser.rows[0]);
+    
+          res.json({ user: newUser.rows[0], token });
+        } catch (e) {
+          console.log(`Ошибка: ${e.message}`);
+          res.status(400).json(`Ошибка: ${e.message}`);
+        }
+      }
+    
+      async loginUser(req, res) {
+        try {
+          const errors = validationResult(req);
+          if (!errors.isEmpty()) {
+            return res.status(400).json(errors.errors.map((e, index) => `${index}. Ошибка:${e.msg}`));
+          }
+    
+          const { name, password } = req.body;
+          const nameSearch = await db.query('SELECT * FROM users WHERE username = $1', [name]);
+    
+          if (nameSearch.rows.length === 0) {
+            return res.status(400).json({ error: `Пользователь ${name} не найден` });
+          }
+    
+          const validPassword = bcrypt.compareSync(password, nameSearch.rows[0].password);
+    
+          if (!validPassword) {
+            return res.status(400).json({ error: `Введен неверный пароль` });
+          }
+    
+          // Генерация JWT-токена после успешного входа
+          const token = await this.generateToken(nameSearch.rows[0]);
+    
+          res.json({ message: 'Вы успешно авторизованы', token });
+        } catch (e) {
+          console.log(`Ошибка: ${e.message}`);
+          res.status(400).json(`Ошибка: ${e.message}`);
+        }
+      }
     async getUser(req,res){
         try {
         const getUsers =  await db.query(`SELECT * FROM users `)
